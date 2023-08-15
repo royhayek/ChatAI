02dd7f456e8582d25f0630409494539f6cb6a0e7
Roy Hayek <royhayek27@gmail.com>
2023-08-15 22:39:27 +0300

    Fixed build error

--- plugins/withAndroidStrategiesPlugin.js
+++ plugins/withAndroidStrategiesPlugin.js
@@ -2,7 +2,13 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withAndroidStrategiesPlugin(config) {
   return withAppBuildGradle(config, config => {
-    config.modResults.contents += 'android { defaultConfig { missingDimensionStrategy "store", "play" } }';
+    config.modResults.contents += `
+            android {
+                defaultConfig {
+                    missingDimensionStrategy "store", "play"
+                }
+            }
+        `;
     return config;
   });
 };
